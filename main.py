592d16b61a76cd93c1aee824af174e8c07920bb9
Evan A <evan@eaguil.com>
2024-08-03 12:47:18 -0500

    Intial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""This program converts temperature from Celsius to Fahrenheit and vice versa
+    
+    Python 3
+    Author: Evan Aguilar
+    Class: BIS 2330 - Computer programming I
+    Date: 04/27/2024
+"""
